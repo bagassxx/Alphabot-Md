c10bc9894c744e33aa8c92a1fd416b92fbcd4778
bagassxx <128472187+bagassxx@users.noreply.github.com>
2023-03-21 18:06:31 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -41,24 +41,24 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/@ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "MR.J" //namabot kalian
+global.ownername = "joyy" //nama kalian
+global.myweb = "tiktok.com/@joyy4uu" //bebas asal jan hapus
+global.youtube = "tada" //bebas asal jan hapus
+global.myweb2 = "tada" //bebas
+global.email = "julionelkores@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "62821931157232"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+"+ownernomer //nmr wa kalian
+global.premium = ['6281260386733'] //premium user
+global.owner = ["6285762219817", "62821931157232"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6285762219817" // nomor wa kalian
+global.ownernomerr = "+6285760386733" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/edca4b513ddf29fd52c0e.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
 global.packname = '© ' + botname //sticker wm ubah
-global.author = 'Created By ' + ownername //sticker wm ganti nama kalian
+global.author = 'Created By ' + MR.J //sticker wm ganti nama kalian
 global.sessionName = 'session'
 //type menu for menu
 global.typemenu = 'templateImage' //
@@ -67,7 +67,7 @@ global.typemenu2 = 'templateImage' //
 global.multiplier = 69
 global.limitawal = {
    premium: "Infinity", //unlimited
-   free: 100
+   free: 15
 }
 global.buttonDefault = [{
    urlButton: {
